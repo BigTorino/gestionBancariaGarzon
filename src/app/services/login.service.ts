7288b281bcb30f553199877c142be88ec06acb6f
BigTorino <bigtorino.rn@gmail.com>
2021-03-12 13:27:38 -0400

    Genero login.service

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor() { }
+}
